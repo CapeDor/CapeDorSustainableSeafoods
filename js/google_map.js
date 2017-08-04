15bf1ac7d46d6c2abf0ffaf324b55bec6071dcf3
Quintin Merlin <merlinqp@mm.st>
2017-08-04 19:24:51 -0300

    fucked with map

--- js/google_map.js
+++ js/google_map.js
@@ -5,7 +5,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
-    var myLatlng = new google.maps.LatLng(45.334632, -64.798142);
+    var myLatlng = new google.maps.LatLng(45.333208, -64.800263);
     // 39.399872
     // -8.224454
     
@@ -30,7 +30,7 @@ function init() {
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
-    var addresses = ['Advocate Harbour'];
+    var addresses = ['365 Driftwood Ln, Advocate Harbour, NS B0M 1A0'];
 
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
