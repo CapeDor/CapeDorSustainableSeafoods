96cfb371d14c3c66ad4dfa49fee251b3c7661114
Jordan Tully <jtully@hey.com>
2020-10-24 14:18:14 -0300

    Updates

---
+++ precache-manifest.5fd61e4ac566a6e0d9b2a0bc894c6673.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9b76c6e5fc5a608fde157e9bdcae279",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dfbd9b5882f6bf01c86",
+    "url": "/static/css/2.7f83aeb6.chunk.css"
+  },
+  {
+    "revision": "cb2712d179f71e104095",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "3dfbd9b5882f6bf01c86",
+    "url": "/static/js/2.0ea6184e.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.0ea6184e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb2712d179f71e104095",
+    "url": "/static/js/main.e6c3c007.chunk.js"
+  },
+  {
+    "revision": "abb6e52401257571d234",
+    "url": "/static/js/runtime-main.ab83e43d.js"
+  }
+]);
